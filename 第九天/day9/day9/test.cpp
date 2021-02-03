1542061402d11e4d8aba061c063e1ed289cf0f2b
殇&璃 <57579119+yanjiakang-maker@users.noreply.github.com>
2021-02-03 08:49:57 +0800

    Add files via upload

---
+++ 第九天/day9/day9/test.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+
+// 1. 另类加法
+
+
+
+// 2. 计算n * m网格从左上到右下一共多少种走法 (只能往右边和下面走)
+void test2() {
+
+}
+
+
+
+int main() {
+    test2();
+
+	return 0;
+}
